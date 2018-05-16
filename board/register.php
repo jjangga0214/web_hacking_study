9647899aa84f9d95d47b41b44fb38c841165abce
rev1c0sm0s <rev1c0sm0s@gmail.com>
2018-05-16 23:59:26 +0900

    Add a register page

--- board/register.php
+++ board/register.php
@@ -20,16 +20,16 @@ if ($_POST['user_id'] && $_POST["user_pw"]) {
     $db_pw = "";
     $db_name = "free_board";
 
-    $db = new mysqli($db_address, $db_id, $db_pw, $db_name);
+    $db = mysqli_connect($db_address, $db_id, $db_pw, $db_name);
 
-    if ($db->connect_error) {
+    if ($db === false) {
         die("while constructing database connection, error occurred" . $db->connect_error);
     }
     $user_id = mysqli_real_escape_string($db, $_POST["user_id"]);
     $user_pw = mysqli_real_escape_string($db, $_POST["user_pw"]);
 
     $sql = "INSERT INTO User(user_id, user_pw) VALUES('$user_id', '$user_pw')";
-    if($db->query($sql) == true) {
+    if(mysqli_query($db, $sql)) {
         echo "Register was success. We would redirect you to the index page in 3 seconds";
         echo "<script>setTimeout(function () {window.location.href = \"index.php\";}, 3000)</script>";
     } else {
