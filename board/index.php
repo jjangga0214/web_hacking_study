34071b4384b7d853532a83c3b5a3ae9467855151
rev1c0sm0s <rev1c0sm0s@gmail.com>
2018-05-20 17:31:21 +0900

    separate login process from index.php

--- board/index.php
+++ board/index.php
@@ -11,33 +11,9 @@ session_start();
         <?php
         if ($_SESSION["logged_in"]) {
             echo("Welcome, " . $_SESSION["user_id"] . ".");
-        } else if ($_POST["user_id"] && $_POST["user_pw"]) {
-            $db_address = "localhost";
-            $db_id = "root";
-            $db_pw = "";
-            $db_name = "free_board";
-
-            $db = new mysqli($db_address, $db_id, $db_pw, $db_name);
-
-            if ($db->connect_error) {
-                die("while constructing database connection, error occurred" . $db->connect_error);
-            }
-
-            /** @noinspection SqlDialectInspection */
-            $sql = "SELECT user_id, user_pw FROM Users";
-            $result = $db->query($sql);
-            while ($row = $result->fetch_assoc()) {
-                if ($row["user_id"] == $_POST["user_id"] && $row["user_pw"] == $_POST["user_pw"]) {
-                    $_SESSION["logged_in"] = true;
-                    $_SESSION["user_id"] = $_POST["user_id"];
-                    echo "<script>window.location.replace(\"./index.php\");</script>";
-                    break;
-                }
-            }
-            echo "Login failed. Your id was not found";
         } else {
             echo("
-            <form action='./index.php' method='post'>
+            <form action='./login.php' method='post'>
                 ID: <input type='text' name='user_id' required>
                 PW: <input type='password' name='user_pw' required>
                 <input type='submit' value='Login'>
---
+++ board/login.php
@@ -0,0 +1,38 @@
+<?php session_start(); ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<?php
+if ($_POST["user_id"] && $_POST["user_pw"]) {
+    $db_address = "localhost";
+    $db_id = "root";
+    $db_pw = "";
+    $db_name = "free_board";
+
+    $db = new mysqli($db_address, $db_id, $db_pw, $db_name);
+
+    if ($db->connect_error) {
+        die("while constructing database connection, error occurred" . $db->connect_error);
+    }
+
+    /** @noinspection SqlResolve */
+    $sql = "SELECT user_id, user_pw FROM Users";
+    $result = $db->query($sql);
+    while ($row = $result->fetch_assoc()) {
+        if ($row["user_id"] == $_POST["user_id"] && $row["user_pw"] == $_POST["user_pw"]) {
+            $_SESSION["logged_in"] = true;
+            $_SESSION["user_id"] = $_POST["user_id"];
+            echo "<script>window.location.replace(\"./index.php\");</script>";
+            break;
+        }
+    }
+    echo "Login failed. Your id was not found";
+} else {
+    echo "Invalid access!";
+}
+?>
+</body>
+</html>
