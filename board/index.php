346833142657d498dec0c566bbd7f4cac6e02094
rev1c0sm0s <rev1c0sm0s@gmail.com>
2018-05-20 18:15:35 +0900

    add border

--- board/index.php
+++ board/index.php
@@ -7,13 +7,13 @@ session_start();
 </head>
 <body>
 <header>
-    <div>
+    <div style='border-bottom: 1px solid black; text-align: right;'>
         <?php
         if ($_SESSION["logged_in"]) {
             echo("Welcome, " . $_SESSION["user_id"] . ".");
         } else {
             echo("
-            <form style='border-bottom: 1px solid black; text-align: right;' action='./login.php' method='post'>
+            <form action='./login.php' method='post'>
                 ID: <input type='text' name='user_id' required>
                 PW: <input type='password' name='user_pw' required>
                 <input type='submit' value='Login'>
